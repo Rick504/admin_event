bf634c05c1cd8764db047124a64abb8e112bb5c3
Rick504 <rick.wes1@gmail.com>
2022-10-29 12:32:37 -0300

    instancia cropp funcionando e upload de imagem tbm

---
+++ src/views/Home/components/photo/UploadPhoto.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+    <label>
+        <input
+            ref="uploadFile"
+            type="file"
+            @change="uploadFile"
+        />
+        <span class="btn-upload-photo">
+            Adicionar Foto
+        </span>
+        <div>
+            <img ref="imgDone" :src="image">
+        </div>
+    </label>
+    </div>
+</template>
+
+<script>
+import Cropper from 'cropperjs';
+
+export default {
+    data() {
+        return {
+            cropper: {},
+            image: null,
+            reader: null
+        }
+    },
+    methods: {
+        uploadFile(e) {
+            this.image = e.target.files[0]
+            this.reader = new FileReader()
+
+            this.reader.readAsDataURL(this.image)
+            this.reader.onload = e =>{ this.image = e.target.result }
+            this.cropper = new Cropper(this.$refs.imgDone, {
+                zoomable: false,
+                scalable: false,
+                aspectRatio: 1
+            })
+        console.log(this.cropper )
+        }
+    }
+}
+</script>
+
+<style scoped>
+img {
+  display: block;
+
+  /* This rule is very important, please don't ignore this */
+  max-width: 100%;
+}
+</style>
