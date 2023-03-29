cba0b7d9b637f900abba5c2a3b21a13f19091255
Rick504 <rick.wes1@gmail.com>
2023-03-28 21:37:32 -0300

    ao editar salvar campo de foto junto ao form

--- src/views/Home/components/dialogs/DialogEdit.vue
+++ src/views/Home/components/dialogs/DialogEdit.vue
@@ -1,67 +1,46 @@
 <template>
+    <v-row>
+        <v-btn color="warning" text dark @click.stop="dialog = true">
+            <v-icon>{{ icons.mdiPencil }}</v-icon>
+            &nbsp;
+            Editar
+        </v-btn>
 
-  <v-row>
-    <v-btn
-      color="warning"
-      text
-      dark
-      @click.stop="dialog = true"
-    >
-      <v-icon>{{ icons.mdiPencil }}</v-icon>
-      &nbsp;
-      Editar
-    </v-btn>
+        <v-dialog dark v-model="dialog" max-width="500">
+            <v-card>
+                <v-container>
+                    <v-row class="text-center">
+                        <v-col>
+                            <div v-if="verifyGeneralAdmin">
+                                <label for="name">Nome:</label>
+                                <v-text-field v-model="form.name" solo>
+                                </v-text-field>
+                            </div>
+                            <div v-if="!this.$route.meta.layout.dialogs.select">
+                                <label for="company">Empresa:</label>
+                                <v-text-field v-model="form.company" solo>
+                                </v-text-field>
+                            </div>
+                        </v-col>
+                    </v-row>
+                    <v-row v-if="this.$route.meta.layout.dialogs.select">
+                        <v-col>
+                            <div for="permissionLevel">Permissionamento:</div>
+                            <v-select :items="permissionLevel" label="Selecionar nível de permissão"
+                                v-model="form.permissionLevel" dense solo></v-select>
+                        </v-col>
+                    </v-row>
+                    <v-row class="text-center">
+                        <v-col>
+                            <v-btn text @click="dialog = false"> Voltar </v-btn>
+                            <v-btn text @click="save"> Salvar </v-btn>
+                        </v-col>
+                    </v-row>
 
-    <v-dialog dark v-model="dialog" max-width="500" >
-        <v-card >
-            <v-container>
-                <v-row class="text-center">
-                    <v-col>
-                        <div
-                            v-if="verifyGeneralAdmin"
-                        >
-                            <label for="name">Nome:</label>
-                            <v-text-field
-                                v-model="form.name"
-                                solo
-                            >
-                            </v-text-field>
-                        </div>
-                        <div
-                            v-if="!this.$route.meta.layout.dialogs.select"
-                        >
-                         <label for="company">Empresa:</label>
-                            <v-text-field
-                                v-model="form.company"
-                                solo
-                            >
-                            </v-text-field>
-                        </div>
-                    </v-col>
-                </v-row>
-                <v-row v-if="this.$route.meta.layout.dialogs.select">
-                    <v-col>
-                        <div for="permissionLevel">Permissionamento:</div>
-                        <v-select
-                            :items="permissionLevel"
-                            label="Selecionar nível de permissão"
-                            v-model="form.permissionLevel"
-                            dense
-                            solo
-                        ></v-select>
-                    </v-col>
-                </v-row>
-                <v-row class="text-center">
-                    <v-col>
-                        <v-btn text @click="dialog = false"> Voltar </v-btn>
-                        <v-btn text @click="save"> Salvar </v-btn>
-                    </v-col>
-                </v-row>
-
-            </v-container>
-        </v-card>
-    </v-dialog>
-  </v-row>
+                </v-container>
+            </v-card>
+        </v-dialog>
+    </v-row>
 </template>
 
 <script>
@@ -90,7 +69,8 @@
             id: null,
             name: '',
             company: '',
-            permissionLevel: ''
+            permissionLevel: '',
+            photo: ''
         }
     }),
     methods: {
@@ -112,6 +92,7 @@
         this.form.name = this.user.name
         this.form.company = this.user.company
         this.form.permissionLevel = this.user.permissionLevel
+        this.form.photo = this.user.photo
     }
   }
 </script>
